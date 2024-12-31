23be8c485d700521e66384eb03f194b71a3617e4
M S <unixwzrd.register@mac.com>
2024-12-31 08:45:10 -0600

    more lint and numpy rebuild

--- bin/numpy-1.26-reinst.sh
+++ bin/numpy-1.26-reinst.sh
@@ -1,3 +1,3 @@
-$!/usr/bin/env Bash
+#!/usr/bin/env bash
 
 CFLAGS="-I/System/Library/Frameworks/vecLib.framework/Headers -Wl,-framework -Wl,Accelerate -framework Accelerate" pip install numpy==1.26.* --force-reinstall --no-deps --no-cache --no-binary :all: --no-build-isolation --compile -Csetup-args=-Dblas=accelerate -Csetup-args=-Dlapack=accelerate -Csetup-args=-Duse-ilp64=true
